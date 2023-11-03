0457af5fd3c9d679a8d3e7acad2b079d467c31b8
BarisYildirim00 <b.yildirimbladeco@gmail.com>
2023-11-03 19:02:43 +0300

    Moin Location Share Added

---
+++ public/js/chat.js
@@ -0,0 +1,79 @@
+const socket = io();
+
+//elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+socket.on("locationMessage", (url) => {
+  console.log(url);
+  const html = Mustache.render(locationMessageTemplate, {
+    url,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  // const message = document.querySelector("input").value;
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`The message was delivered!-->`);
+  });
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $sendLocationButton.removeAttribute("disabled");
+        console.log("Location Shared!");
+      }
+    );
+  });
+});
+
+//----------------------------------------------------------------
+// socket.on("countUpdated", (count) => {
+//   console.log("The count has been updated!", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//   console.log("Clicked");
+//   socket.emit("increment");
+// });
---
+++ src/index.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", "Welcome");
+
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+
+    io.emit("message", message);
+    callback("Delivered!");
+  });
+
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+    callback();
+  });
+
+  socket.on("disconnet", () => {
+    io.emit("message", "A user has left");
+  });
+
+  //----------------------------------------------------------------
+
+  //   socket.emit("countUpdated", count);
+
+  //   socket.on("increment", () => {
+  //     count++;
+  //     io.emit("countUpdated", count);
+  //   });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
